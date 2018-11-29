64b5e723d363417e4addfb0e94ef1df00f2a2840
Nick Robison <nicholas.a.robison@omb.eop.gov>
2018-11-29 14:45:34 -0500

    Now building libmacaroons as either static or dynamic, based on the presence of the library in the system.
    It also handles versions 'more' correctly. It stil gets confused if the package
    is found on the system, but too old to use.

--- cli/cmake/Findlibmacaroons.cmake
+++ cli/cmake/Findlibmacaroons.cmake
@@ -2,34 +2,26 @@
 
 find_package(PkgConfig)
 
-message("Finding pkg")
-
 pkg_check_modules(PC_LIBMACAROONS QUIET libmacaroons)
 set(LIBMACAROONS_DEFINITIONS ${PC_LIBMACAROONS_CFLAGS_OTHER})
 
-message("Finding path")
-
-find_path(LIBMACAROONS_INCLUDE_DIR
+find_path(libmacaroons_INCLUDE_DIR
         NAMES macaroons.h
         HINTS ${PC_LIBMACAROONS_INCLUDE_DIRS} ${PC_LIBMACAROONS_INCLUDEDIR})
 
-find_library(LIBMACAROONS_LIBRARY NAMES macaroons
+find_library(libmacaroons_LIBRARY NAMES macaroons
         HINTS ${PC_LIBMACAROONS_LIBDIR} ${PC_LIBMACAROONS_LIBRARY_DIRS})
 
+
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(libmacaroons DEFAULT_MSG
-        LIBMACAROONS_INCLUDE_DIR LIBMACAROONS_LIBRARY)
+find_package_handle_standard_args(libmacaroons
+        REQUIRED_VARS libmacaroons_INCLUDE_DIR libmacaroons_LIBRARY
+        VERSION_VAR PC_LIBMACAROONS_VERSION)
 
-mark_as_advanced(LIBMACAROONS_INCLUDE_DIR LIBMACAROONS_LIBRARY)
-
-if (LIBMACAROONS_FOUND AND NOT TARGET libmacaroons::libmacaroons)
-    add_library(libmacaroons::libmacaroons UNKNOWN IMPORTED)
-    set_target_properties(libmacaroons::libmacaroons PROPERTIES
-            IMPORTED_LOCATION "${LIBMACAROONS_LIBRARY}"
-            INTERFACE_INCLUDE_DIRS "${LIBMACAROONS_INCLUDE_DIR}")
+# Set everything up
+if (libmacaroons_FOUND AND NOT TARGET libmacaroons)
+    add_library(libmacaroons UNKNOWN IMPORTED)
+    set_target_properties(libmacaroons PROPERTIES
+            IMPORTED_LOCATION "${libmacaroons_LIBRARY}"
+            INTERFACE_INCLUDE_DIRS "${libmacaroons_INCLUDE_DIR}")
 endif ()
-
-
-set(LIBMACAROONS_LIBRARIES ${LIBMACAROONS_LIBRARY})
-set(LIBMACAROONS_INCLUDE_DIRS ${LIBMACAROONS_INCLUDE_DIR})
-
