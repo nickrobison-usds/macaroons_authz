49946f1b47a4083e6baba8e8895316b28b896592
Nick Robison <nicholas.a.robison@omb.eop.gov>
2018-12-11 12:23:19 -0500

    Added initial support for seeding the database in Docker.
    Need a way to create/destroy the actual DB, right now, we can only seed it.

--- playbooks/authz-entrypoint.sh
+++ playbooks/authz-entrypoint.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
 
-echo 'Running'
 export GO_ENV=production
+
+# Check to see if we need to run the seed process
+if [ -n "$SEED" ]
+then
+   echo 'Initializing and seeding the database'
+   # Run the migration
+   cms_authz_linux migrate
+
+   # Seed the data
+   cms_authz_linux task db:seed
+fi
+
+echo 'Starting server in prod mode'
 exec "$@"
