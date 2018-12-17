ba22eb826ebf95dbd88818f86479f17adcdc94bb
Nick Robison <nicholas.a.robison@omb.eop.gov>
2018-12-17 15:31:04 -0500

    Java service now decodes multiple macaroons from the Cookie header.

--- java/src/main/java/com/nickrobison/cmsauthz/resources/RootAPIResource.java
+++ java/src/main/java/com/nickrobison/cmsauthz/resources/RootAPIResource.java
@@ -1,15 +1,22 @@
 package com.nickrobison.cmsauthz.resources;
 
 import com.codahale.metrics.annotation.Timed;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.type.TypeFactory;
 import com.github.nitram509.jmacaroons.Macaroon;
 import com.github.nitram509.jmacaroons.MacaroonsBuilder;
 import com.github.nitram509.jmacaroons.MacaroonsVerifier;
+import org.apache.commons.codec.binary.Base64;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.*;
 import javax.ws.rs.core.*;
+import java.io.IOException;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Path("/")
 @Produces(MediaType.APPLICATION_JSON)
@@ -37,15 +44,22 @@ public class RootAPIResource {
     @Timed
     public Response get(@Context HttpServletRequest request, @PathParam("aco_id") String aco_id, @Context HttpHeaders headers) {
 
-        final Optional<Macaroon> macaroon = this.getMacaroonFromHeader(headers);
+        final Optional<List<Macaroon>> macaroons = this.getMacaroonFromHeader(headers);
 
-        if (!macaroon.isPresent()) {
+        if (!macaroons.isPresent()) {
             return Response.status(Response.Status.UNAUTHORIZED).entity("Must have Macaroon").build();
         }
 
-        final MacaroonsVerifier verifier = new MacaroonsVerifier(macaroon.get())
-                .satisfyExcact("aco_id = 1");
-        final boolean valid = verifier.isValid(TEST_KEY);
+        final MacaroonsVerifier initialVerifier = new MacaroonsVerifier(macaroons.get().get(0));
+
+//        Add all the discharges
+        final MacaroonsVerifier verifier = macaroons.get().subList(1, macaroons.get().size())
+                .stream()
+                .reduce(initialVerifier, MacaroonsVerifier::satisfy3rdParty, (acc, mac) -> acc);
+
+        final boolean valid = verifier
+                .satisfyExcact("aco_id = 1")
+                .isValid(TEST_KEY);
 
         if (valid) {
             return Response.ok("Hello there!").build();
@@ -58,10 +72,11 @@ public class RootAPIResource {
     /**
      * Get Macaroon from header.
      * If it's missing, that should fail the auth.
+     *
      * @param headers - {@link HttpHeaders} headers from request
      * @return - {@link Optional} {@link Macaroon} from header
      */
-    private Optional<Macaroon> getMacaroonFromHeader(HttpHeaders headers) {
+    private Optional<List<Macaroon>> getMacaroonFromHeader(HttpHeaders headers) {
         final Map<String, Cookie> cookies = headers.getCookies();
         Optional<Cookie> firstMacaroon = cookies
                 .entrySet()
@@ -70,6 +85,42 @@ public class RootAPIResource {
                 .map(Map.Entry::getValue)
                 .findFirst();
 
-        return firstMacaroon.map(cookie -> MacaroonsBuilder.deserialize(cookie.getValue()));
+        // Base64 decode the input
+        return firstMacaroon
+                .map(Cookie::getValue)
+                .map(RootAPIResource::parseMacaroons)
+                .map(macs -> macs.stream().map(MacaroonsBuilder::deserialize).collect(Collectors.toList()));
+    }
+
+    private static List<String> parseMacaroons(String macaroon) {
+        final Base64 base64 = new Base64();
+        final byte[] decoded = base64.decode(macaroon);
+//        Figure out what we're looking at.
+        switch (decoded[0]) {
+//            JSON v2
+            case '{': {
+                throw new IllegalArgumentException("Cannot decode V2 JSON");
+            }
+            // V2 Binary
+            case 0x02: {
+                throw new IllegalArgumentException("Cannot decode V2 Binary");
+            }
+//            If we're an array, maybe do things.
+            case '[': {
+//                Assume we're an array of JSON, and bail
+                if (decoded[1] == '{') {
+                    throw new IllegalArgumentException("Cannot decode array of V2 JSON");
+                }
+            }
+        }
+
+        // Now we know that we're an array of V1 Macaroons, so make them a json array and split them apart.
+        final ObjectMapper objectMapper = new ObjectMapper();
+        final TypeFactory typeFactory = objectMapper.getTypeFactory();
+        try {
+            return objectMapper.readValue(decoded, typeFactory.constructCollectionType(List.class, String.class));
+        } catch (IOException e) {
+            throw new IllegalStateException(e);
+        }
     }
 }
